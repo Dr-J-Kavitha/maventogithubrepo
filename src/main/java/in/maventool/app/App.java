5e46abc52f0b0b4563502b67204d05a1b64ae6e3
Dr-J-Kavitha <drkavithareddyaws@gmail.com>
2024-06-19 12:37:24 +0530

    local repo committed

---
+++ src/main/java/in/maventool/app/App.java
@@ -0,0 +1,13 @@
+package in.maventool.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
